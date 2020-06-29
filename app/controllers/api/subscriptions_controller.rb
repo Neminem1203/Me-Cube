08bf8e70e6f6b92053c680d8e52b3429d6d70dd2
neminem1203 <tpaul1203@gmail.com>
2020-06-29 12:41:55 -0400

    Removed some stuff in subscriptions controller for heroku to work

--- app/controllers/api/subscriptions_controller.rb
+++ app/controllers/api/subscriptions_controller.rb
@@ -1,12 +1,7 @@
 class Api::SubscriptionsController < ApplicationController
     def index
+
         
-        if !params[:subscriber_id]:
-            return
-        elsif !params[:channel_id]:
-            return
-        else:
-            return
     end
     
     def create
